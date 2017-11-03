5d3545962d66f041549fa96a4e2b2c2880509398
Nguyen Duy Cuong <cuongnd.ami@outlook.com>
2017-11-03 18:04:52 +0700

    header

--- cuongnd.info/MyProfile/src/app/app.module.ts
+++ cuongnd.info/MyProfile/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import {MatMenuModule, MatIconModule, MatButtonModule, MatSnackBarModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,BrowserAnimationsModule,
+    MatMenuModule, MatIconModule, MatButtonModule, MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
